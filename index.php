51d2eaa30404b9d8498a9f4f85207b7192201c98
Мигель <83421739+Mihel17@users.noreply.github.com>
2022-03-16 10:47:43 +0200

    watch

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html class="page" lang="ru">
+<?php require 'layout/TEST/GET_FROM_BACKEND_head.php'; ?>
+
+<body class="page__body">
+  <?php require 'layout/TEST/GET_FROM_BACKEND_header.php'; ?>
+
+  <main class="page__main  main-content  main-content--pricing">
+    <?php require 'layout/TEST/GET_FROM_BACKEND_breadcrumbs.php'; ?>
+
+
+    <div class="container  container--pricing">
+      <h1 class="gradient  title  title--pricing">PRICING</h1>
+      <!-- H1 -->
+      <?php require 'layout/section/pricing.php'; ?>
+      <!-- SECTION-PRICING -->
+    </div>
+    <!-- CONTAINER-PRICING -->
+  </main>
+  <!-- MAIN-CONTENT -->
+
+
+  <?php require 'layout/TEST/GET_FROM_BACKEND_footer.php'; ?>
+</body>
+</html>
---
+++ layout/section/pricing.php
@@ -0,0 +1,89 @@
+<section class="pricing">
+  <ul class="pricing__list">
+
+    <li class="pricing__item">
+      <div class="pricing__item-title  title-style">
+        <div class="title-style__animation"></div>
+        <h2>Seamless <br> Wallet API</h2>
+      </div>
+      <!-- pricing__item-title -->
+      <p class="pricing__item-description">Fast integration of iGaming content to your brand.</p>
+      <p class="pricing__item-offer">7% from GGR*</p>
+      <p class="pricing__item-features-title">features</p>
+      <ol class="pricing__item-features">
+        <li class="pricing__item-feature">Lifetime updates</li>
+        <li class="pricing__item-feature">No platform fee</li>
+        <li class="pricing__item-feature">48-hour integration</li>
+      </ol>
+      <a class="pricing__btn  btnMore" href="#">contact sales</a>
+    </li>
+    <!-- pricing__item -->
+
+    <li class="pricing__item">
+      <div class="pricing__item-title  title-style">
+        <div class="title-style__animation"></div>
+        <h2>Turnkey</h2>
+      </div>
+      <!-- pricing__item-title -->
+      <p class="pricing__item-description">Fast integration of iGaming content to your brand.</p>
+      <p class="pricing__item-offer">7% from GGR*</p>
+      <p class="pricing__item-features-title">features</p>
+      <ol class="pricing__item-features">
+        <li class="pricing__item-feature">Cutting edge design</li>
+        <li class="pricing__item-feature">Full functional delivery</li>
+        <li class="pricing__item-feature">Personal account manager</li>
+      </ol>
+      <a class="pricing__btn  btnMore" href="#">contact sales</a>
+    </li>
+    <!-- pricing__item -->
+
+    <li class="pricing__item">
+      <div class="pricing__item-title  title-style">
+        <div class="title-style__animation"></div>
+        <h2>Sport <br> Engine</h2>
+      </div>
+      <!-- pricing__item-title -->
+      <p class="pricing__item-description">Fast integration of iGaming content to your brand.</p>
+      <p class="pricing__item-offer">7% from GGR*</p>
+      <p class="pricing__item-features-title">features</p>
+      <ol class="pricing__item-features">
+        <li class="pricing__item-feature">Fully customizable product</li>
+        <li class="pricing__item-feature">Ready-to-use technology</li>
+        <li class="pricing__item-feature">Instant brand integration</li>
+      </ol>
+      <a class="pricing__btn  btnMore" href="#">contact sales</a>
+    </li>
+    <!-- pricing__item -->
+
+    <li class="pricing__item">
+      <div class="pricing__item-title  title-style">
+        <div class="title-style__animation"></div>
+        <h2>SRetail</h2>
+      </div>
+      <!-- pricing__item-title -->
+      <p class="pricing__item-description">Fast integration of iGaming content to your brand.</p>
+      <p class="pricing__item-offer">7% from GGR*</p>
+      <p class="pricing__item-features-title">features</p>
+      <ol class="pricing__item-features">
+        <li class="pricing__item-feature">Powerful reports and management tools for admins
+        </li>
+        <li class="pricing__item-feature">Easy-to-use and functional panel for cashiers</li>
+        <li class="pricing__item-feature">User-friendly platform interface for players</li>
+      </ol>
+      <a class="pricing__btn  btnMore" href="#">contact sales</a>
+    </li>
+    <!-- pricing__item -->
+
+  </ul>
+  <!-- pricing__list -->
+
+  <p class="pricing__message">
+    * The pricing may vary depending on the desired product characteristics.
+    <a class="pricing__message-link" href="#">
+      solutions comparison
+    </a>
+  </p>
+  <!-- pricing__message -->
+
+</section>
+<!-- pricing -->
